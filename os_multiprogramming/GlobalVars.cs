816879366642a554da0b1f18c7974a2a8a2ddec2
deadlorpa <dealorpa@gmail.com>
2021-10-05 16:20:06 +0700

    тест №1

--- os_multiprogramming/GlobalVars.cs
+++ os_multiprogramming/GlobalVars.cs
@@ -8,11 +8,11 @@ namespace os_multiprogramming
 {
     public static class GlobalVars
     {
-        public static int MAXIMUM_DURATION = 60;  // секунды
-        public static int MAXIMUM_DURATION_IO = 10; // секунды
+        public static int MAXIMUM_DURATION = 100;  // секунды
+        public static int MAXIMUM_DURATION_IO = 50; // секунды
         public static int PROCESSOR_TICK = 1000;
-        public static double PROBABILITY_IO = 0.2;
-        public static double PROBABILITY_NEW_TASK = 0.3;
+        public static double PROBABILITY_IO = 0.10;
+        public static double PROBABILITY_NEW_TASK = 0.16;
         public enum TaskStates { RUN, WAIT, COMPLETE, IO };
     }
 }
